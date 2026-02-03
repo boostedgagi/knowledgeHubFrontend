a02096f999699ca84a72086e1be69ad86d630fec
boostedgagi <dragan.02jelic@gmail.com>
2026-02-03 18:42:08 +0100

    main.js implementation of pinia

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import {createPinia} from "pinia";
 
-const app = createApp(App).use(router);
+const app = createApp(App);
+
+app.use(router);
+app.use(createPinia());
 
 app.config.globalProperties.api = 'http://localhost:8000'
 
